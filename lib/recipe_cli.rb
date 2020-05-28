323e03c3e88047dff37c19c393ecfd85c5faf642
ginelle <plaisirginelle@gmail.com>
2020-05-27 20:50:31 -0400

    WIP:Changed filename to reflect new API

---
+++ lib/recipe_cli.rb
@@ -0,0 +1,11 @@
+#frozen_string_literal :true
+
+#application environment
+require 'bundler/setup'
+Bundler.require(:default)
+
+require 'dotenv/load'
+
+require_relative './recipe_cli/cli.rb'
+require_relative './recipe_cli/api.rb'
+require_relative './recipe_cli/recipe.rb'
