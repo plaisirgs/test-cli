16074a5f6a34c633ca1e0dec39061a5f5f3a9505
ginelle <plaisirginelle@gmail.com>
2020-05-28 20:52:51 -0400

    WIP: Updated meal_categories method in api.rb

--- lib/recipe_cli/cli.rb
+++ lib/recipe_cli/cli.rb
@@ -1,4 +1,27 @@
 
 class Cli 
+
+    def greeting
+        puts "Hello user!"
+        #display the meal_categories 
+        #What category would you like?
+        #collect user selection then use present_meal method
+    end
+
+
+    def present_meal(meal_id)
+        puts "You have selected the #strMeal recipe." #will call the API using the meal_id
+        #
+    end
+
     puts "Cli class loaded"
 end
+
+#based on user input, interacts with user
+
+
+#greeting
+#enjoy your meal
+
+
+#error message if user selects a category that is not on the list --> maybe show user the list again and ask to choose 
--- lib/recipe_cli/recipe.rb
+++ lib/recipe_cli/recipe.rb
@@ -1,5 +1,8 @@
-#frozen_string_literal :true
+# frozen_string_literal: true
 
 class Recipe
     puts "Recipe class loaded"
 end
+
+#attributes that I want to show the user
+attr_accessor :ingredients, YouTube link, name, area(ex.Mexican)
