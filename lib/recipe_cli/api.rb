47a46db15ffac5b55f47c8889ff81d411ee7fff8
ginelle <plaisirginelle@gmail.com>
2020-05-28 20:52:51 -0400

    WIP: Updated meal_categories method in api.rb

--- lib/recipe_cli/api.rb
+++ lib/recipe_cli/api.rb
@@ -1,7 +1,32 @@
-#frozen_string_literal :true
+# frozen_string_literal: true
 
 class Api
-    puts "Api class loaded"
+    BASE_URL = 'https://www.themealdb.com/api/json/v1/1/'
+
+    def meal_categories
+        categories = []
+        category_number = 1
+        res = RestClient.get(BASE_URL + 'categories.php')
+        data = JSON.parse(res.body)
+        data["categories"].each do |category|
+        if category["strCategory"] == "Miscellaneous" || category["strCategory"] == "Side" || category["strCategory"] == "Starter"
+           next 
+        else
+           binding.pry
+        categories << category_number.to_s + ". " + category["strCategory"] 
+        category_number += 1
+        end
+        end 
+        categories
+    end
+
+    def meal_from_meal_id(meal_id)
+        #
+    end
+
+    def 
+
 end
 
-KEY = ENV['API_KEY']
+#Meal Categories -> Category -> Meal ID
+#provides data for CLI
